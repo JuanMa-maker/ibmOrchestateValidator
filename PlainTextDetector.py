5e5613457f3361f779d2755b0d7954beb25bf758
JuanMa-Maker <jsanchezvalaz@gmail.com>
2025-11-23 10:58:58 -0600

    final

--- PlainTextDetector.py
+++ PlainTextDetector.py
@@ -4,9 +4,16 @@ import io
 import pypdf as pdf_reader
 from datetime import datetime, timedelta
 
+headers_reales = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Connection": "keep-alive"
+}
+
 def pdf_checker(url_file: str):
     try:
-        connection = requests.get(url_file)
+        connection = requests.get(url_file, headers=headers_reales, timeout=10)
         connection.raise_for_status()
         reader = pdf_reader.PdfReader(io.BytesIO(connection.content))
         
